8fcbbd136e26dd2e57603ffe669e789313e7ed18
root <root@gitserver.local>
2019-11-16 21:42:36 +0700

    Tampolin Online by Agit naeta

---
+++ precache-manifest.d71cbd71d843923048c83554498d1f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f6720f7573710fb9d229d99a51cbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681f2b9ea7c6d8019f92",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "8eab1625cf2e870ef51f",
+    "url": "/static/css/main.63814504.chunk.css"
+  },
+  {
+    "revision": "681f2b9ea7c6d8019f92",
+    "url": "/static/js/2.3e785666.chunk.js"
+  },
+  {
+    "revision": "8eab1625cf2e870ef51f",
+    "url": "/static/js/main.fa84f12d.chunk.js"
+  },
+  {
+    "revision": "37cebeb7c2b006f31526",
+    "url": "/static/js/runtime-main.7c8919ed.js"
+  }
+]);
